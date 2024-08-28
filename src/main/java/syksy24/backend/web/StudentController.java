b028a8eab12a58f84b2dd091ef716552273b00c3
Phoolis <carlsonps@gmail.com>
2024-08-28 19:08:48 +0300

    Add static list of students and GetMapping for studentList

--- src/main/java/syksy24/backend/web/StudentController.java
+++ src/main/java/syksy24/backend/web/StudentController.java
@@ -1,5 +1,18 @@
 package syksy24.backend.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import syksy24.backend.domain.Student;
+
 public class StudentController {
 
+    private static List<Student> studentList = new ArrayList<>();
+
+    static {
+        studentList.add(new Student("Pekka", "Pouta"));
+        studentList.add(new Student("Kyösti", "Pöysti"));
+        studentList.add(new Student("Maurizio", "Undercover"));
+    }
+    
 }
